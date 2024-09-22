bf89dcfe02202810cd48edd19cd0dbdcd1e35b03
max925001 <shivam969684@gmail.com>
2024-09-22 23:53:47 +0530

    changes in client

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,40 +1,29 @@
-import "../3d.css";
-
-import image1 from "../assets/slider/1.png";
-import image2 from "../assets/slider/2.png";
-import image3 from "../assets/slider/3.png";
-import image4 from "../assets/slider/4.jpg";
-import image5 from "../assets/slider/5.png";
-import image6 from "../assets/slider/6.png";
-import image7 from "../assets/slider/7.png";
+import React from 'react';
+import image1 from '../assets/slider/1.png';
+import image2 from '../assets/slider/2.png';
+import image3 from '../assets/slider/3.png';
+import image4 from '../assets/slider/4.jpg';
+import image5 from '../assets/slider/5.png';
+import image6 from '../assets/slider/6.png';
+import image7 from '../assets/slider/7.png';
 
 const images = [image1, image2, image3, image4, image5, image6, image7];
 
 const Slider = () => {
   return (
-    <div className="banner">
-      <div className="slider" style={{ "--quantity": 7 }}>
-        <div className="item" style={{ "--position": 1 }}>
-          <img src={images[0]} alt="Dragon 1" />
-        </div>
-        <div className="item" style={{ "--position": 2 }}>
-          <img src={images[1]} alt="Dragon 2" />
-        </div>
-        <div className="item" style={{ "--position": 3 }}>
-          <img src={images[2]} alt="Dragon 3" />
-        </div>
-        <div className="item" style={{ "--position": 4 }}>
-          <img src={images[3]} alt="Dragon 4" />
-        </div>
-        <div className="item" style={{ "--position": 5 }}>
-          <img src={images[4]} alt="Dragon 5" />
-        </div>
-        <div className="item" style={{ "--position": 6 }}>
-          <img src={images[5]} alt="Dragon 6" />
-        </div>
-        <div className="item" style={{ "--position": 7 }}>
-          <img src={images[6]} alt="Dragon 7" />
-        </div>
+    <div className="relative w-full overflow-hidden bg-white py-4 sm:py-6 md:py-8">
+      <div className="flex animate-scroll-once whitespace-nowrap space-x-4 sm:space-x-6 md:space-x-8 lg:space-x-10 px-2 sm:px-4 md:px-6">
+        {images.map((image, index) => (
+          <div 
+            key={index} 
+            className="flex-shrink-0 w-28 h-28 sm:w-36 sm:h-36 md:w-48 md:h-48 lg:w-56 lg:h-56 xl:w-64 xl:h-64">
+            <img
+              src={image}
+              alt={`Partner ${index + 1}`}
+              className="w-full h-full object-contain"
+            />
+          </div>
+        ))}
       </div>
     </div>
   );
